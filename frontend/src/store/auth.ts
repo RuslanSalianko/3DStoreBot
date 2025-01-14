d670f4ad656b419f975fce4d3567e05cffd6ed39
Ruslan Salianko <r.salianko@gmail.com>
2025-01-14 19:08:10 +0300

    refactor(frontend): refactor auth services

--- frontend/src/store/auth.ts
+++ frontend/src/store/auth.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 
 import { UserService } from '@services/user.service';
-import { AuthService } from './service/auth';
+import { AuthService } from '@services/auth.service';
 
 import { IUser } from '@models/user.interface';
 import { IAuthResponse } from '@models/response/auth';
