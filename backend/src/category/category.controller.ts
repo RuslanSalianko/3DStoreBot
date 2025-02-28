a0d3b4dbaa015102620e855cb1f269639793db00
Ruslan Salianko <r.salianko@gmail.com>
2025-02-28 11:31:14 +0300

    feat(backend): added create category

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Controller('category')
 export class CategoryController {
@@ -10,5 +11,11 @@ export class CategoryController {
   @Get()
   async categories() {
     return this.categoryService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async create(@Body() body: CreateCategoryDto) {
+    return this.categoryService.create(body);
   }
 }
--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { Category } from '@prisma/client';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Injectable()
 export class CategoryService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateCategoryDto): Promise<Category> {
+    return this.prisma.category.upsert({
+      where: {
+        name: data.name,
+      },
+      create: {
+        name: data.name,
+      },
+      update: {},
+    });
+  }
 
   async findAll(): Promise<Category[]> {
     return this.prisma.category.findMany();
